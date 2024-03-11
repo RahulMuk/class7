a4217ec4e9f6e2fcd612c452c3ea0c64c1e0f000
Rahul Mukherjee <r.mukherjee5@icloud.com>
2024-03-11 00:06:00 -0400

    Initial commit of files

---
+++ script_empty.js
@@ -0,0 +1,70 @@
+    // Select all the filters
+    
+    // dropdownFilter
+    // Radio Button Filter
+    // Checkbox Filter
+    // Price Button
+
+    // select the item elements
+    
+  
+    // filter by color (Dropdown)
+
+    // get the color values
+
+    // loop through each element
+
+        // get the color property of the element
+
+        // if all is selected or if the value of the filter matches the item, display block (visible)
+
+        // else display none (hidden)
+
+
+
+    // filter by size (radio buttons)
+
+    // get the radio button values
+
+    // loop through each element
+
+        // get the size property of the element
+
+        // if all is selected or if the value of the filter matches the item, display block (visible)
+
+        // else display none (hidden)
+
+
+    // filter by type (checkbox)
+
+     // get checkbox values, put in array
+
+    // loop through each element
+
+        // get the type property of the element
+
+        // if there are no boxes selected or the type of the element is in the array, display block (visible)
+
+        // else display none (hidden)
+
+
+    // filter by price (Button)
+
+
+    // loop through each element
+
+        // get the price property of the element
+
+        // if all is selected or if the value of the filter matches the item, display block (visible)
+
+        // else display none (hidden)
+
+
+
+
+    
+
+    //Add event listeners for each filter
+  
+
+  
